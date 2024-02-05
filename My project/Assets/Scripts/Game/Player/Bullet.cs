b23dac94a154ed2705f3d9681f2bc2de3f6821fa
Frederick <antvenomxd72@gmail.com>
2024-02-04 23:34:01 -0300

    feat: player can shoot

---
+++ My project/Assets/Scripts/Game/Player/Bullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.GetComponent<EnemyMovement>()) {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ My project/Assets/Scripts/Game/Player/PlayerShoot.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _bulletPrefab;
+
+    [SerializeField]
+    private float _bulletSpeed;
+
+    private bool _fireContinuously;
+    [SerializeField]
+    private Transform _gunOffset;
+    [SerializeField]
+    private float _shotDelay;
+    private float _lastFireTime;
+    // Update is called once per frame
+    void Update()
+    {
+        if (_fireContinuously) {
+            float timeSinceLastFire = Time.time - _lastFireTime;
+
+            if (timeSinceLastFire >= _shotDelay) {
+                FireBullet();
+
+                _lastFireTime = Time.time;
+            }
+        }
+    }
+
+    private void OnFire(InputValue inputvalue) {
+        _fireContinuously = inputvalue.isPressed;
+
+    }
+
+    private void FireBullet() {
+        GameObject bullet = Instantiate(_bulletPrefab, _gunOffset.position, transform.rotation);
+
+        Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
+
+        rigidbody.velocity = _bulletSpeed * transform.up;
+    }
+}
