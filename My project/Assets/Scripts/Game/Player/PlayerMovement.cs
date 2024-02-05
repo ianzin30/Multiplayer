311e1fa46dbcb55a7d551790b9749bba181dbcae
Frederick <antvenomxd72@gmail.com>
2024-02-04 21:02:27 -0300

    feat: Player Object can move with a modifiable speed

---
+++ My project/Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movementInput;
+    private void Awake() {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        _rigidbody.velocity = _movementInput * _speed;
+    }
+
+    // updates whenever the player object moves
+    private void OnMove(InputValue inputValue) {
+        _movementInput = inputValue.Get<Vector2>();
+    }
+    
+    
+}
