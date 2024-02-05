5e1b047a47f7d318e7f5b26305b976b593ec04b3
Frederick <antvenomxd72@gmail.com>
2024-02-04 22:04:35 -0300

    feat: player rotates in the direction of the movement

--- My project/Assets/Scripts/Game/Player/PlayerMovement.cs
+++ My project/Assets/Scripts/Game/Player/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 5;
+    [SerializeField]
+    private float _rotationSpeed;
     private Rigidbody2D _rigidbody;
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
@@ -17,9 +19,18 @@ public class PlayerMovement : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate() {
+    // updated every frame
+    private void FixedUpdate()
+    {
+        SetPlayerVelocity();
+        RotateInDirectionOfInput();
+    }
+
+    private void SetPlayerVelocity()
+    {
+        // smoothing is used to avoid sudden movements
         _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput, ref _movementInputSmoothVelocity, _accelerationDelay);
-        _rigidbody.velocity = _smoothedMovementInput * _speed;
+        _rigidbody.velocity = _smoothedMovementInput * _speed; // updates velocity
     }
 
     // updates whenever the player object moves
@@ -27,5 +38,14 @@ public class PlayerMovement : MonoBehaviour
         _movementInput = inputValue.Get<Vector2>();
     }
     
+    private void RotateInDirectionOfInput() {
+        // check if the player is moving
+        if (_movementInput != Vector2.zero) {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+            _rigidbody.MoveRotation(rotation);
+        }
+    }
     
 }
