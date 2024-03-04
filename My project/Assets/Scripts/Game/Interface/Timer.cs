a7b101e610f4d0432d6b1fb433c0263234199771
Frederick <antvenomxd72@gmail.com>
2024-03-03 22:32:58 -0300

    feat: add timer

---
+++ My project/Assets/Scripts/Game/Interface/Timer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
+    private int minutes;
+    private int seconds;
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        minutes = Mathf.FloorToInt(elapsedTime / 60);
+        seconds = Mathf.FloorToInt(elapsedTime % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
